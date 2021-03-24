d9a9a6226b4e23e6df0e857f481d1d1c109e1ffd
eryzna <eryzna@gmail.com>
2021-03-24 08:24:16 -0700

    scrape_sitings and scrape_images

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,3 +1,110 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
+    def initialize
+        @journey_north_urls = [
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2015&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2015&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2016&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2016&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2017&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2017&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2018&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2018&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2019&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2019&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2020&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2020&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=spring&map=monarch-adult-spring&year=2021&submit=View+Data',
+            'https://journeynorth.org/sightings/querylist.html?season=fall&map=monarch-adult-fall&year=2021&submit=View+Data'
+        ]
+    end
+    
+    def scrape_sitings
+        
+        sitings = []
+        @journey_north_urls.each do |url|
+            
+            html = open(url)
+            
+            doc = Nokogiri::HTML(html)
+
+            sitings_scrape = doc.css('.querylist').css('tbody').css('tr')
+
+            sitings_scrape.each do |siting|
+                td = siting.css('td')
+                date = td[1].text
+                town = td[2].text
+                state_province = td[3].text
+                num_of_individuals = td[6].text
+                if date[7] == '5'
+                    year_id = 7
+                elsif date[7] == '6'
+                    year_id = 6
+                elsif date[7] == '7'
+                    year_id = 5
+                elsif date[7] == '8'
+                    year_id = 4
+                elsif date[7] == '9'
+                    year_id = 3
+                elsif date[7] == '0'
+                    year_id = 2
+                else 
+                    year_id = 1
+                end
+        
+                siting_info = {
+                    date: date, 
+                    town: town, 
+                    state_province: state_province, 
+                    num_of_individuals: num_of_individuals,
+                    year_id: year_id
+                    #season_id: season_id
+                }
+                sitings << siting_info 
+            end
+        end
+        binding.pry
+        sitings
+    end
+
+
+    def scrape_images
+        
+        images = []
+        @journey_north_urls.each do |url|
+        
+            html = open(url)
+     
+            doc = Nokogiri::HTML(html)
+
+            sitings_scrape = doc.css('.querylist').css('tbody').css('tr')
+
+            sitings_scrape.each do |siting|
+                td = siting.css('td')
+                img_url = td[7].css('img').attribute('src').value
+
+                imgs_info = {
+                    img_url: img_url
+                }
+
+                images << imgs_info if imgs_info[:img_url] != "/maps/graphics/spacer.gif"
+            #binding.pry
+            end
+        #binding.pry
+        #sitings
+        end
+        #binding.pry
+        images
+    end
 end
+#binding.pry
+
+
+scrape = Scraper.new
+scrape.scrape_sitings
+#scrape.scrape_images
+#ruby app/models/scraper.rb
