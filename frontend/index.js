da264bcd31f237567fcb70af4fde3b7feb7d6b47
eryzna <eryzna@gmail.com>
2021-04-09 15:25:48 -0400

    trying to fix filter form

--- frontend/index.js
+++ frontend/index.js
@@ -9,14 +9,14 @@ let years = []
 let citySightings = []
 
 const states = [
-  'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 
+  '','AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 
   'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 
   'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
   'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
   'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' 
 ]
 
-// Adds states to dropdown
+// States
 function addStates() {
   let dropdown = document.getElementById('state-dropdown')
 
@@ -28,42 +28,34 @@ function addStates() {
   }
 }
 
-//Event listener for state select 
 function addStateSelectListener() {
   let filter = document.getElementById('state-filter')
-  let input = document.getElementById('city-search')
   let dropdown = document.getElementById('state-dropdown')
   filter.addEventListener('click', function () {
-    let city = input.value.charAt(0).toUpperCase() + input.value.slice(1);
-    filterSightingsByCity(city)
-
-    filterSightingsByState(dropdown.value);
-    
-    //
-    //console.log(dropdown.value)
-    //console.log(input.value)
+      filterSightingsByState(dropdown.value);
   });
 
 }
 
-function filterSightingsByCity(city) {
-  citySightings = sightings.filter(sighting => sighting.town === city);
-  console.log(citySightings)
-}
+//function filterSightingsByCity(city) {
+  //citySightings = sightings.filter(sighting => sighting.town === city);
+  //console.log(citySightings)
+  //let dropdown = document.getElementById('state-dropdown')
+  //filterSightingsByState(dropdown.value)
+  //console.log(dropdown.value)
+//}
 
-// Filters sightings by state
+ //Filters sightings by state
 function filterSightingsByState(state) {
-  console.log(state)
-  console.log(citySightings)
-  updateSightingList(citySightings.filter(sighting => sighting.state_province === state));
+    updateSightingList(sightings.filter(sighting => sighting.state_province === state));
 }
 
-function searchCity() {
-  let input = document.getElementById('city-search').value
-  let city = input.charAt(0).toUpperCase() + input.slice(1);
+//function searchCity() {
+  //let input = document.getElementById('city-search').value
+  //let city = input.charAt(0).toUpperCase() + input.slice(1);
   //filterSightingsByCity(city)
   //updateSightingList(sightings.filter(sighting => sighting.town === city))
-}
+//}
 
 function updatePage(sightings) {
   let grid = document.getElementById('sightings-grid')
@@ -78,8 +70,7 @@ function updatePage(sightings) {
   grid.appendChild(itemTwo)
 }
               
-// Loads sightings into the DOM
-
+// Sightings
 function loadSightingOptions() {
     const sightingUrl = 'http://localhost:3000/sightings'
     fetch(sightingUrl)
@@ -94,18 +85,6 @@ function loadSightingOptions() {
     });
 } 
 
-function loadYearOptions() {
-  const yearUrl = 'http://localhost:3000/years';
-  fetch (yearUrl)
-    .then(res => res.json())
-    .then(results => {
-      years = results
-      addYearSelectListener();
-    
-    });
-}
-
-
 function updateSightingList(sightings) {
   let ul = document.querySelector('#sighting-info');
   let info = document.getElementById('app-info')
@@ -115,19 +94,26 @@ function updateSightingList(sightings) {
   updatePage(sightings)
 }
 
-function removeChildren(element) {
-  let child = element.lastElementChild;
-  while (child) {
-    element.removeChild(child);
-    child = element.lastElementChild;
-  }
+function addSighting(sighting) {
+  let ul = document.querySelector('#sighting-info');
+  let li = document.createElement('li');
+  li.innerText = `Date: ${sighting.date}, City: ${sighting.town}, State/Province: ${sighting.state_province}, Number of Monarchs: ${sighting.num_of_individuals}`
+  //li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  //li.addEventListener('click', updateColor);
 }
+//
 
-function filterSightingsByYear(year) {
-  let yearsFilter = years.filter(y => y.value == year);
-  let yearsReduce = yearsFilter.reduce(year => sightings)
-  sightings = yearsReduce.sightings
-  updateSightingList(sightings)
+//Years
+function loadYearOptions() {
+  const yearUrl = 'http://localhost:3000/years';
+  fetch (yearUrl)
+    .then(res => res.json())
+    .then(results => {
+      years = results
+      addYearSelectListener();
+    
+    });
 }
 
 function addYearSelectListener() {
@@ -140,11 +126,27 @@ function addYearSelectListener() {
   });
 }
 
-function addClearParamsListener () {
-  var clear = document.getElementById('clear-params')
-  var dropdown = document.getElementById('year-dropdown')
-  var input = document.getElementById('city-search')
-  var ul = document.querySelector('#sighting-info');
+function filterSightingsByYear(year) {
+  let yearsFilter = years.filter(y => y.value == year);
+  let yearsReduce = yearsFilter.reduce(year => sightings)
+  sightings = yearsReduce.sightings
+  updateSightingList(sightings)
+}
+//
+
+function addFilterSelectListener () {
+
+}
+
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+function addAppInfo () {
   var info = document.getElementById('app-info')
   var grid = document.getElementById('sightings-grid')
   var h2 = document.createElement('h2')
@@ -152,101 +154,44 @@ function addClearParamsListener () {
   var p = document.createElement('p')
   var pText = document.createTextNode("Please select your search parameters or record a sighting.")
 
+  //removeChild(info);
+  
+
+  h2.appendChild(text);
+  p.appendChild(pText);
+  info.appendChild(h2)
+  info.appendChild(p);
+}
+
+
+function addClearParamsListener () {
+  var clear = document.getElementById('clear-params')
+  var yearDropdown = document.getElementById('year-dropdown')
+  var stateDropdown = document.getElementById('state-dropdown')
+  var input = document.getElementById('city-search')
+  var ul = document.querySelector('#sighting-info');
+  var grid = document.getElementById('sightings-grid')
+  var info = document.getElementById('app-info')
+  
   //let p = document.createElement('p').value = "Please select your search parameters or record a sighting."
     
   clear.addEventListener('click', function() {
-    dropdown.value = "None"
+    yearDropdown.value = "None"
+    stateDropdown.value = ""
     input.value = ""
     
+    removeChildren(info);
     removeChildren(ul);
     removeChildren(grid);
-    h2.appendChild(text)
-    p.appendChild(pText)
-    info.appendChild(h2)
-    info.appendChild(p)
-    //removeChildren(info);
-    //clearParams(dropdown.value)
-    //dropdown.value = "None"
+    addAppInfo();
+  
+    addStates();
+    loadSightingOptions();
+    loadYearOptions();
+    
   })
 }
 
-function addSighting(sighting) {
-  let ul = document.querySelector('#sighting-info');
-  let li = document.createElement('li');
-  li.innerText = `Date: ${sighting.date}, City: ${sighting.town}, State/Province: ${sighting.state_province}, Number of Monarchs: ${sighting.num_of_individuals}`
-  //li.style.cursor = 'pointer';
-  ul.appendChild(li);
-  //li.addEventListener('click', updateColor);
-}
-//function filterSightingsByYear() {
-  //document.getElementById("sighting-info").innerHTML = sightings.filter(yearFilter);
-//}
 
-//function yearFilter() {
-  //return year = document.getElementById("year-dropdown").value
-  //debugger
-//}
-
-//function checkAdult(age) {
-  //return age >= document.getElementById("ageToCheck").value;
-//}
-
-//console.log('%c HI', 'color: firebrick')
-////let breeds = [];
-
-//document.addEventListener('DOMContentLoaded', function () {
-//  loadImages();
-//  loadBreedOptions();
-//});
-
-//function loadImages() {
-//  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-//  fetch(imgUrl)
-//    .then(res=> res.json())
-//    .then(results => {
-//      results.message.forEach(image => addImage(image))
-//    });
-//}
-
-//function addImage(dogPicUrl) {
-//  let container = document.querySelector('#dog-image-container');
-//  let newImageEl = document.createElement('img');
-//  newImageEl.src = dogPicUrl;
-//  container.appendChild(newImageEl);
-//}
-
-//function loadBreedOptions() {
-//  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-//  fetch(breedUrl)
-//    .then(res => res.json())
-//    .then(results => {
-//
-//      breeds = Object.keys(results.message);
-//      updateBreedList(breeds);
-//      addBreedSelectListener();
-//    });
-//}
-
-
-
-// function updateColor(event) {
-//  event.target.style.color = 'lightgreen';
-
-//<p>Minimum age: <input type="number" id="ageToCheck" value="18"></p>
-//<button onclick="myFunction()">Try it</button>
-
-//<p>All ages above minimum: <span id="demo"></span></p>
-
-//<script>
-//var ages = [32, 33, 12, 40];
-
-//function checkAdult(age) {
-//  return age >= document.getElementById("ageToCheck").value;
-//}
-
-//function myFunction() {
-//  document.getElementById("demo").innerHTML = ages.filter(checkAdult);
-//}
-//</script>
 
 
